8529990db50995c76728acb5be0b0e71ff6f1fa7
Marlon <marlomendes0910@gmail.com>
2025-10-28 17:05:18 -0300

    :sparkles: feat: Adição de funções, score e perguntas para o quiz

--- Projetos_Python/Quiz/questions.py
+++ Projetos_Python/Quiz/questions.py
@@ -1,39 +1,43 @@
 q1 = {
     'Question' : '1. Normalmente, quantos litros de sangue uma pessoa tem?\n',
     'Alternatives' : 'a) Tem entre 2 a 4 litros.\nb) Tem entre 4 a 6 litros.\nc) Tem 10 litros.\nd) Tem 7 litros.\ne) Tem 0,5 litros.\n',
+    'Explanation': 'A quantidade de sangue varia de pessoa para pessoa. O volume de sangue é entre 7% e 8% o peso corporal. Assim, um adulto com 50 e 80 quilos, pode ter entre 4 e 6 litros de sangue, respectivamente. Portanto, 4 a 6 litros é a alternativa que compreende a quantidade mais próxima de adultos com pesos diferentes.',
     'Answer' : 'b'
 }
-
 q2 = {
     'Question' : '2. De quem é a famosa frase “Penso, logo existo”?\n',
-    'Alternatives' : 'a) Platão \nb) Galileu Galilei \nc) Descartes \nd) Sócrates \ne)  Francis Bacon\n',
+    'Alternatives' : 'a) Platão \nb) Galileu Galilei \nc) Descartes \nd) Sócrates \ne) Francis Bacon\n',
+    'Explanation': '"Je pense, donc je suis" é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.',
     'Answer' : 'c'
 }
 q3 = {
     'Question' : '3. De onde é a invenção do chuveiro elétrico?\n',
     'Alternatives' : 'a) França \nb) Inglaterra \nc) Brasil \nd) Austrália \ne) Itália\n',
+    'Explanation':'Francisco Canhos é o nome do homem que, na década de 40, desenvolveu o primeiro chuveiro elétrico seguro em Jaú-SP. O aparelho vinha sendo desenvolvido desde os anos 30, mas apresentava riscos de curto-circuito.',
     'Answer' : 'c'
 }
-
-list_questions = [q1,q2,q3]
-""" 
 q4 = {
-    'Question' : '1. Normalmente, quantos litros de sangue uma pessoa tem?',
-    'Alternatives' : 'a)Tem entre 2 a 4 litros.\nb)Tem entre 4 a 6 litros.\nc)Tem 10 litros.\nd)Tem 7 litros.\ne)Tem 0,5 litros.\n',
-    'Answer' : 'b'
+    'Question' : '4. Quais o menor e o maior país do mundo?',
+    'Alternatives' : 'a) Vaticano e Rússia \nb) Nauru e China \nc) Mônaco e Canadá \nd) Malta e Estados Unidos \ne) São Marino e Índia',
+    'Explanation': 'O Vaticano, sede da igreja católica, tem apenas 44 hectares (0,44 km2). A Rússia, localizada em dois continentes (Ásia e Europa), tem 17 milhões de km2.',
+    'Answer' : 'a'
 }
 q5 = {
-    'Question' : '1. Normalmente, quantos litros de sangue uma pessoa tem?',
-    'Alternatives' : 'a)Tem entre 2 a 4 litros.\nb)Tem entre 4 a 6 litros.\nc)Tem 10 litros.\nd)Tem 7 litros.\ne)Tem 0,5 litros.\n',
-    'Answer' : 'b'
+    'Question' : '5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'Alternatives' : 'a) Jânio Quadros \nb) Jacinto Anjos \nc) Getúlio Vargas \nd) João Figueiredo \ne) João Goulart\n',
+    'Explanation': 'João Belchior Marques Goulart (1919-1976) foi o 24º presidente do Brasil, cujo período de governo compreende os anos de 1961 a 1964.',
+    'Answer' : 'e'
 }
 q6 = {
-    'Question' : '1. Normalmente, quantos litros de sangue uma pessoa tem?',
-    'Alternatives' : 'a)Tem entre 2 a 4 litros.\nb)Tem entre 4 a 6 litros.\nc)Tem 10 litros.\nd)Tem 7 litros.\ne)Tem 0,5 litros.\n',
+    'Question' : '6. Qual o grupo em que todas as palavras foram escritas corretamente?',
+    'Alternatives' : 'a) Asterístico, beneficiente, meteorologia, entertido \nb) Asterisco, beneficente, meteorologia, entretido \nc) Asterisco, beneficente, metereologia, entretido \nd) Asterístico, beneficiente, metereologia, entretido \ne) Asterisco, beneficiente, metereologia, entretido\n',
+    'Explanation': 'Estas palavras são exemplos de barbarismo, um vício de linguagem em que as palavras são pronunciadas ou escritas incorretamente.',
     'Answer' : 'b'
 }
 q7 = {
-    'Question' : '1. Normalmente, quantos litros de sangue uma pessoa tem?',
-    'Alternatives' : 'a)Tem entre 2 a 4 litros.\nb)Tem entre 4 a 6 litros.\nc)Tem 10 litros.\nd)Tem 7 litros.\ne)Tem 0,5 litros.\n',
+    'Question' : '7. Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Alternatives' : 'a) O Senhor dos Anéis \nb) Dom Quixote \nc) O Pequeno Príncipe \nd) Ela, a Feiticeira \ne) Um Conto de Duas Cidades\n',
+    'Explanation': 'Dom Quixote, de Miguel de Cervantes, é um clássico da literatura espanhola. A obra foi escrita em duas partes, uma em 1605, e a outra em 1615.',
     'Answer' : 'b'
-} """
+} 
+list_questions = [q1,q2,q3,q4,q5,q6,q7]
--- Projetos_Python/Quiz/quiz.py
+++ Projetos_Python/Quiz/quiz.py
@@ -1,15 +1,25 @@
 from questions import list_questions
 
-print("Seja muito bem vindo(a) ao quiz do Marlon!")
+print("\nSeja muito bem vindo(a) ao quiz do Marlon!")
 
 answer_user = input("Vamos começar? (S/N)\n")
-if answer_user != 'S':
-    quit();
+if answer_user.upper() != 'S':
+    quit()
+score = 0
+print("\nComeçando... \n");
+for item in list_questions:
+    answer_user = input(f"{item['Question']}\n{item['Alternatives']} \n")
+    if answer_user.lower() == item['Answer']:
+        print("Resposta Certa!\n")
+        score+=1
+    else:
+        print("Resposta Errada!\n")
+        print(f"A resposta correta é '{item['Answer']}'.\n")
+
+    print(f"{item['Explanation']}\n")
+if(score==0):
+    print("Estude mais um pouco.")
 else:
-    print("Começando...");
-    for item in list_questions:
-        answer_user = input(f"{item['Question']}{item['Alternatives']}")
-        if answer_user == item['Answer']:
-            print("Resposta Certa!\n")
-        else:
-            print("Resposta Errada!\n")
+    print("Muito bem!")
+print(f"Você acertou {score}/{len(list_questions)} \n")
+print("Finalizando...\n")
